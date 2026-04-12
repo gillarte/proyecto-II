52e07639757c3850ee421edbe401086a4a2f63dd
Thiago D <thiagodecaraescuela1418@gmail.com>
2026-04-12 02:37:27 -0300

    Add files via upload

---
+++ area/codigo.ts
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Ingrese un la medida del lado 1:"));
+let numero2 = Number(prompt("Ingrese un la medida del lado 2:"));
+let area = numero * numero2;
+if (area > 25) {
+    alert("El area es mayor a 25");
+}
+else{
+    alert("El area no es mayor a 25");
+}
